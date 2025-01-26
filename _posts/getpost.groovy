f48e01e465b155fd47cd758af18ec7ca15b92a02
Egon Willighagen <egon.willighagen@gmail.com>
2025-01-26 11:13:03 +0100

    Newer Bacting

--- _posts/getpost.groovy
+++ _posts/getpost.groovy
@@ -1,9 +1,9 @@
-// Copyright (c) 2023  Egon Willighagen <egon.willighagen@gmail.com>
+// Copyright (c) 2023-2025  Egon Willighagen <egon.willighagen@gmail.com>
 //
 // GPL v3
 
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.3.4')
-@Grab(group='io.github.egonw.bacting', module='net.bioclipse.managers.jsoup', version='0.3.4')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='1.0.4')
+@Grab(group='io.github.egonw.bacting', module='net.bioclipse.managers.jsoup', version='1.0.4')
 
 bioclipse = new net.bioclipse.managers.BioclipseManager("..");
 ui = new net.bioclipse.managers.UIManager("..");
